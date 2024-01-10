13546946a66377145adf8da9e2f9f7a4511caad5
nabelly <anabellymontibeller@gmail.com>
2024-01-10 16:19:56 -0300

    test arma and chefe

---
+++ Arma.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+public abstract class Arma 
+{
+    public string Nome { get; set; }
+    public int Nivel { get; set; }
+    public int Ataque { get; set; }
+
+    public virtual void Atirar() {}
+    public virtual void Recarregar() {}
+    public virtual void Melhorar() {}
+
+}
