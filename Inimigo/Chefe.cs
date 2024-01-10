535c3011b82fa89efb1dd308fbab21d5db981d65
Lander <lander.gerotto@gmail.com>
2024-01-10 17:00:18 -0300

    Changes on Sound

--- Inimigo/Chefe.cs
+++ Inimigo/Chefe.cs
@@ -1,20 +1,20 @@
-using System;
-using System.Windows.Forms;
-using System.Drawing;
+// using System;
+// using System.Windows.Forms;
+// using System.Drawing;
 
-public class Chefe1
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-    private Image img;
-    private DateTime danoRecebido = DateTime.MinValue;
-    public Chefe1()
-    {
+// public class Chefe1
+// {
+//     public int X { get; set; }
+//     public int Y { get; set; }
+//     private Image img;
+//     private DateTime danoRecebido = DateTime.MinValue;
+//     public Chefe1()
+//     {
 
-    }
+//     }
 
-    public void DanoRecebido()
-    {
-        danoRecebido = DateTime.Now;
-    }
-}
+//     public void DanoRecebido()
+//     {
+//         danoRecebido = DateTime.Now;
+//     }
+// }
