5500cfb260bd5a0e5639004dde797ffd7e6e7f86
nabelly <anabellymontibeller@gmail.com>
2024-01-10 17:00:39 -0300

    add lanca

--- Arma.cs
+++ Arma.cs
@@ -6,7 +6,7 @@ public abstract class Arma
 {
     public string Nome { get; set; }
     public int Nivel { get; set; }
-    public int Ataque { get; set; }
+    public int Dano { get; set; }
 
     public virtual void Atirar() {}
     public virtual void Recarregar() {}
---
+++ NivelArma/Lanca.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+public class Lanca : Arma
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Tamanho { get; set; }
+    private Image img;
+    public Lanca()
+        {
+            this.Nivel = 1;
+            this.Dano = 2;
+            this.Nome = "Lança Benzida Pela Igraja Católica";
+            this.img = Bitmap.FromFile("../Sprites/Lanca/lanca.png");
+
+            X = 100;
+            Y = 100;
+        }
+    
+    public void Draw(Graphics g)
+    {
+        g.TranslateTransform(X, Y);
+        
+    }
+
+
+}
