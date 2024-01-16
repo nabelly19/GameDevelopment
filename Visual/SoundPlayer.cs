0510b3d35ca10ccc4412c124d5d4f19347ebbbd9
benhur feld <benhurfeld@gmail.com>
2024-01-16 14:49:21 -0300

    Small rework (and half commented)

--- Visual/SoundPlayer.cs
+++ Visual/SoundPlayer.cs
@@ -5,8 +5,8 @@ public class SoundPlayer
     public SoundPlayer()
     {
         this.song = new System.Media.SoundPlayer();
-        song.SoundLocation = "./Midia/songs/OMORI OST - 106 GOLDENVENGEANCE.wav"; // DEBUG MODE
-        // song.SoundLocation = "./Midia/songs/OMORI OST - 106 GOLDENVENGEANCE.wav";
+        // song.SoundLocation = "../../../assets/songs/OMORI OST - 106 GOLDENVENGEANCE.wav"; // DEBUG MODE
+        song.SoundLocation = "./assets/songs/OMORI OST - 106 GOLDENVENGEANCE.wav";
     }
 
     public void Play() => this.song.PlayLooping();
