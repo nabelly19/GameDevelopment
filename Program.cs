ab09b36ba93600864a3af4b4c8ec4fe60cb90c1f
benhur feld <benhurfeld@gmail.com>
2024-01-16 14:49:56 -0300

    Whole Revamp woking

--- Program.cs
+++ Program.cs
@@ -3,16 +3,15 @@ using System.CodeDom;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-
 ApplicationConfiguration.Initialize();
 
 Bitmap bmp = null;
 Graphics g = null;
 
-Game.Current.Sound = new SoundPlayer();
-Game.Current.CreateDungeons(g);
-Game.Current.BossList.Add(new Boss("./Midia/Sprites/Bosses/pxArt.png", 10, 10));
-Game.Current.Player = new Player("./Midia/Sprites/Player/download.png", 25, 25);
+GameEngine engine = new();
+engine.AddObject(new Boss("Ele", 500, 500, "./assets/Sprites/Bosses/pxArt.png"));
+engine.AddObject(new Player("Ele", 0, 0, "./assets/Sprites/Player/download.png"));
+
 
 var pb = new PictureBox { Dock = DockStyle.Fill, };
 
@@ -33,21 +32,13 @@ form.Load += (o, e) =>
     g.Clear(Color.Black);
     pb.Image = bmp;
     timer.Start();
-    Game.Current.CurrentMap.CreateWalls(pb);
-    Game.Current.Sound.Play();
+    engine.StartSound();
 };
 
 timer.Tick += (o, e) =>
 {
     g.Clear(Color.Black);
-    Game.Current.DrawMap(g, pb);
-    Game.Current.Player.Move();
-    Game.Current.PlayerBossColision(
-            Game.Current.BossList[0], 
-            Game.Current.Player
-        );
-    Game.Current.BossList[0].Draw(g);
-    Game.Current.Player.Draw(g);
+    engine.Render(g, pb);
 
     pb.Refresh();
 };
@@ -61,25 +52,25 @@ form.KeyDown += (o, e) =>
             break;
 
         case Keys.W:
-            Game.Current.Player.MoveUp();
+            engine.player.MoveUp();
             break;
 
         case Keys.A:
 
-            Game.Current.Player.MoveLeft();
+            engine.player.MoveLeft();
             break;
 
         case Keys.S:
-            Game.Current.Player.MoveDown();
+            engine.player.MoveDown();
             break;
 
         case Keys.D:
-            Game.Current.Player.MoveRight();
+            engine.player.MoveRight();
             break;
 
-        case Keys.Space:
-            Game.Current.CurrentMap.UpdateBackground("./Midia/Maps/dungeon_pre.png");
-            break;
+        // case Keys.Space:
+        //     Game.Current.CurrentMap.UpdateBackground("./Midia/Maps/dungeon_pre.png");
+        //     break;
     }
 };
 
@@ -88,19 +79,19 @@ form.KeyUp += (o, e) =>
     switch (e.KeyCode)
     {
         case Keys.W:
-            Game.Current.Player.Velocity_Y = 0;
+            engine.player.Vy = 0;
             break;
 
         case Keys.A:
-            Game.Current.Player.Velocity_X = 0;
+            engine.player.Vx = 0;
             break;
 
         case Keys.S:
-            Game.Current.Player.Velocity_Y = 0;
+            engine.player.Vy = 0;
             break;
 
         case Keys.D:
-            Game.Current.Player.Velocity_X = 0;
+            engine.player.Vx = 0;
             break;
     }
 };
