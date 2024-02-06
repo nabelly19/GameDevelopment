213f93001ef6685fa674059266aa44142a88ec2c
benhuuur <127983364+benhuuur@users.noreply.github.com>
2024-02-06 16:23:14 -0300

    Redo platform state

--- State/States.cs
+++ State/States.cs
@@ -204,63 +204,78 @@ public class PlatformState : State
 {
     float durationTime;
     DateTime creationTime = DateTime.Now;
+    float lifetime;
     Platform platform;
+    int random;
     bool isActivated = false;
     PointF[] spawns =
     {
-        new PointF(465, 390),
-        new PointF(465, 910),
-        new PointF(1450, 390),
-        new PointF(1450, 910),
+        new PointF(
+            Screen.PrimaryScreen.Bounds.Width * 0.2421875f,
+            Screen.PrimaryScreen.Bounds.Height * 0.361111111f
+        ),
+        new PointF(
+            Screen.PrimaryScreen.Bounds.Width * 0.2421875f,
+            Screen.PrimaryScreen.Bounds.Height * 0.8425925826f
+        ),
+        new PointF(
+            Screen.PrimaryScreen.Bounds.Width * 0.7552083333f,
+            Screen.PrimaryScreen.Bounds.Height * 0.361111111f
+        ),
+        new PointF(
+            Screen.PrimaryScreen.Bounds.Width * 0.7552083333f,
+            Screen.PrimaryScreen.Bounds.Height * 0.8425925826f
+        ),
     };
 
     public PlatformState(float lifetime)
     {
-        var random = Random.Shared.Next(0, 2);
-        platform = new("Platform", spawns[random].X, spawns[random].Y, 290, 260);
-        if (GameEngine.Current.Player.X > Screen.PrimaryScreen.Bounds.Width / 2)
-            platform = new("Platform", spawns[random + 2].X, spawns[random + 2].Y, 290, 260);
-        this.durationTime = lifetime + platform.damageInitiationDelay;
+        this.lifetime = lifetime;
     }
 
     public override void Act(Boss boss)
     {
-        int contador = 2;
-
         if (!isActivated)
         {
+            random = Random.Shared.Next(0, 2);
+            if (GameEngine.Current.Player.X > Screen.PrimaryScreen.Bounds.Width / 2)
+                random += 2;
+
+            this.platform = new(
+                "Platform",
+                spawns[random].X,
+                spawns[random].Y,
+                Screen.PrimaryScreen.Bounds.Width * 0.1510416667f,
+                Screen.PrimaryScreen.Bounds.Height * 0.2407407407f
+            );
+
             creationTime = DateTime.Now;
-            platform.creationTime = DateTime.Now;
+            platform.creationTime = creationTime;
             GameEngine.Current.AddObjectToCollisionList(platform);
             isActivated = true;
         }
+
+        if (random == 0 && platform.isAttaking)
+            MapManager.Current.SetBackground(Resources.Maps[2]);
+
+        else if (random == 1 && platform.isAttaking)
+            MapManager.Current.SetBackground(Resources.Maps[4]);
+
+        else if (random == 2 && platform.isAttaking)
+            MapManager.Current.SetBackground(Resources.Maps[3]);
+
+        else if (random == 3 && platform.isAttaking)
+            MapManager.Current.SetBackground(Resources.Maps[5]);
+
+        this.durationTime = this.lifetime + platform.damageInitiationDelay;
         var diff = DateTime.Now - creationTime;
         var millis = (float)diff.TotalMilliseconds;
-
         if (millis > durationTime)
         {
+            random = 0;
             isActivated = false;
+            MapManager.Current.SetBackground(Resources.Maps[1]);
             CollisionManager.RemoveGameObject(platform);
-            var random = Random.Shared.Next(0, 2);
-            platform = new("Platform", spawns[random].X, spawns[random].Y, 290, 260);
-            if (GameEngine.Current.Player.X > Screen.PrimaryScreen.Bounds.Width / 2)
-                platform = new("Platform", spawns[random + 2].X, spawns[random + 2].Y, 290, 260);
-
-            if (random == 0)
-                MapManager.Current.SetBackground(Resources.Maps[2]);
-
-
-            if (random == 1 + contador)
-                MapManager.Current.SetBackground(Resources.Maps[3]);
-
-            if (random == 0 + contador)
-                MapManager.Current.SetBackground(Resources.Maps[4]);
-
-
-            if (random == 1)
-                MapManager.Current.SetBackground(Resources.Maps[5]);
-
-
             GoToNext();
             return;
         }
